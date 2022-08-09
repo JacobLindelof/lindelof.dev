482d7447efa95549d03c8ab100802e857be3708f
Jacob Lindelof <jlindelof@me.com>
2022-08-09 14:13:29 -0400

    working deployment script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,6 @@ module.exports = {
       host: ['lindelof.dev'],
       ref: 'origin/master',
       repo: 'git@github.com:Unholypanda/lindelof.dev.git',
-      ssh_options: ['ForwardAgent=yes'],
       path: '/home/jlindelof/lindelof.dev/',
       'post-deploy' : 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
     }
